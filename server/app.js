2e14d5217497e48d0e1bbdff944f4968ce2e5dd4
Evan Lesmez <ELesmez@in.deliveryagent.com>
2018-07-26 12:01:54 -0400

    changed files

--- server/app.js
+++ server/app.js
@@ -2,12 +2,12 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
-
+const mLab = require('./mLabcredz');
 const app = express();
 
 
 
-mongoose.connect('mongodb://evan:test123@ds141611.mlab.com:41611/gql-evan');
+mongoose.connect(mLab.credz,{useNewUrlParser: true});
 mongoose.connection.once('open', () => {
     console.log('Connected to database');
 });
